cb711dc7b6c0154a3522816726513bb70e41b545
Carlo Field <carlo@swiss.dev>
2026-01-16 17:40:19 +0100

    feat: pre-commit for flake
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- flake.nix
+++ flake.nix
@@ -1,15 +1,58 @@
 {
-  description = "A very basic flake";
+  description = "wireguard-router";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    {
+      self,
+      systems,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      formatter = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          config = self.checks.${system}.pre-commit-check.config;
+          inherit (config) package configFile;
+          script = ''
+            ${pkgs.lib.getExe package} run --all-files --config ${configFile}
+          '';
+        in
+        pkgs.writeShellScriptBin "pre-commit-run" script
+      );
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      checks = forEachSystem (system: {
+        pre-commit-check = inputs.git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt.enable = true;
+          };
+        };
+      });
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      devShells = forEachSystem (system: {
+        default =
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+          in
+          pkgs.mkShell {
+            inherit shellHook;
+            buildInputs = enabledPackages;
+            packages = [
+              pkgs.rustc
+              pkgs.cargo
+            ];
+          };
+      });
+    };
 }
